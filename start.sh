e2f2cf1db3f96dac2b3d6e77f1873f3a48ec9eff
ssbabu95 <babu.sumeet1@gmail.com>
2015-11-16 17:10:58 -0500

    Start script

---
+++ start.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+PWD=${PWD}
+git clone https://github.com/ssbabu95/cs4501-models.git
+docker run -d --name models -p 8001:8000 -v $PWD/cs4501-models:/app --link mysql:db tp33/django:1.1 mod_wsgi-express start-server cs4501/wsgi.py
+docker exec models python manage.py makemigrations
+docker exec models python manage.py migrate
+docker run -d --name kafka --env ADVERTISED_HOST=kafka --env ADVERTISED_PORT=9092 spotify/kafka
+docker run -d -p 9200:9200 --name es elasticsearch:2.0 -Des.network.host=es
+git clone https://github.com/ssbabu95/cs4501-exp.git
+docker run -d --name exp -p 8002:8000 -v $PWD/cs4501-exp:/app --link models:models-api --link kafka:kafka --link es:es tp33/django:1.1 mod_wsgi-express start-server cs4501/wsgi.py
+git clone https://github.com/ssbabu95/cs4501-web.git
+docker run -d --name web -p 8000:8000 -v $PWD/cs4501-web:/app --link exp:exp-api tp33/django:1.1 mod_wsgi-express start-server cs4501/wsgi.py
+git clone https://github.com/ssbabu95/batch.git
+docker run -it --name batch -v $PWD/batch:/app --link kafka:kafka --link es:es tp33/django:1.1
