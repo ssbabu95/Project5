f5e89e61912e974466195e5fb4f6f9e653e7f9ad
Alexander Goossens <ag4wj@virginia.edu>
2015-12-16 17:29:06 -0500

    python-memcache

--- start.sh
+++ start.sh
@@ -9,8 +9,8 @@ docker run -d -p 9200:9200 --name es elasticsearch:2.0 -Des.network.host=es
 git clone https://github.com/ssbabu95/cs4501-exp.git
 docker run -d --name exp -p 8002:8000 -v $PWD/cs4501-exp:/app --link models:models-api --link kafka:kafka --link es:es tp33/django:1.1 mod_wsgi-express start-server cs4501/wsgi.py
 git clone https://github.com/ssbabu95/cs4501-web.git
-pip install python-memcached
 docker run --name my-memcache -d memcached
 docker run -d --name web -p 8000:8000 -v $PWD/cs4501-web:/app --link exp:exp-api --link mymemcache:memcache tp33/django:1.1 mod_wsgi-express start-server cs4501/wsgi.py
+docker exec pip install python-memcached
 git clone https://github.com/ssbabu95/batch.git
 docker run -it --name batch -v $PWD/batch:/app --link kafka:kafka --link es:es tp33/django:1.1
